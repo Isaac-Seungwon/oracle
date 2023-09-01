ad7f77d160be20468ef42a3ae446c3492ae1cb6c
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-09-01 11:58:40 +0900

    6일차 SQL

--- ex20_join.sql
+++ ex20_join.sql
@@ -277,7 +277,8 @@ create table tblVideo (
     genre number not null references tblGenre(seq)  --장르 번호(FK)
 );
 
-
+-- 대여를 최소 1번 이상 했던 회원과 대여 내역
+	
 -- 고객 테이블
 create table tblMember (
     seq number primary key,         --고객번호(PK)
@@ -299,14 +300,304 @@ create table tblRent (
     remark varchar2(500) null                           --비고
 );
 
+-- 비디오 + 장르 > 조인
+SELECT
+	m.name,
+	v.name,
+	g.price,
+	r.rentdate
+FROM tblGenre g
+	INNER JOIN tblVideo v
+		ON g.seq = v.genre
+			INNER JOIN tblRent r
+				ON v.seq = r.video
+					INNER JOIN tblMember M
+						ON m.seq = r.member;
+	
+-- 기존의 4개의 테이블을 합쳐서 시스템을 구축하였고, 한 눈에 요약해서 보여준다.
+-- 어떤 회원이 어떤 영화를 어떤 가격에 언제 빌려갔는지를 확인하고 있다.
+	
+
+SELECT
+	e.first_name || ' ' || e.last_name AS 직원명,
+	d.department_name AS 부서명,
+	l.city AS 도시명,
+	c.country_name AS 국가명,
+	r.region_name AS 대륙명,
+	j.job_title AS 직업
+FROM employees e
+	INNER JOIN departments d
+		ON d.department_id = e.department_id
+			INNER JOIN locations l
+				ON l.location_id = d.location_id
+					INNER JOIN countries c
+						ON c.country_id = l.country_id
+							INNER JOIN regions r
+								ON r.region_id = c.region_id
+									INNER JOIN jobs j
+										ON j.job_id = e.job_id;
+
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM locations;
+SELECT * FROM countries;
+SELECT * FROM regions;
+SELECT * FROM jobs;
+
+
+
+/*
+	3. 외부 조인 (OUTER JOIN)
+	- 이름만 보면 내부 조인의 반댓말인 거 같지만, 그렇지 않다.
+	- 모든 외부 조인은 내부 조인의 결과를 포함하고 있다. 여기에 +Alpha를 한 것이다.
+	- 내부 조인을 하면 일부 컬럼이 제외되는데, 그 제외된 컬럼 중에 부모 테이블의 나머지 레코드를 합하는 조인이다.
+	- 즉, 외부 조인은 내부 조인에 포함되지 않은 조인을 가리킨다.
+	
+	select
+		컬럼리스트
+	from 테이블A
+		(left|right) outer join 테이블B
+			on 테이블A.컬럼 = 테이블B.컬럼;
+*/
+
+
+SELECT * FROM tblCustomer; --3명 > 5명
+SELECT * FROM tblSales; --9건
+
+COMMIT;
+insert INTO tblCustomer VALUES (4, '호호호', '010-1234-1234', '서울시');
+insert INTO tblCustomer VALUES (5, '이순신', '010-1234-1234', '서울시');
+
+--내부 조인
+SELECT
+	*
+FROM tblCustomer c
+	INNER JOIN tblsales s
+		ON c.seq = s.cseq;
+-- 내부 조인을 하자 결과가 9건이 나왔다.
+-- 이를 업무상으로 표현하자면 물건을 한 번이라도 구매한 이력이 있는 고객의 정보의 그 고객이 사간 구매 내역을 가져오시오라는 의미이다.
+-- 여기서 핵심 포인트는 '물건을 한 번이라도 구매'했다는 점이다. 이게 내부 조인을 의미한다.
+-- 새로 생성한 '호호호', '이순신'데이터가 없다. 이 둘이 없는 이유는 지금 막 가입을 하였고, 판매 테이블에 고객 번호가 저장이 되지 않았기 때문이다.
+-- 부모 테이블과 자식 테이블에 동시에 존재해야 출력이 된다.
+	
+--외부 조인
+--left 조인
+SELECT
+	*
+FROM tblCustomer c
+	left outer JOIN tblsales s
+		ON c.seq = s.cseq; --11	
+-- 외부 조인은 기본적으로 내부 조인을 포함하며, 물건 구매 이력이 없는 2명도 출력한다.
+-- 외부 조인을 실행하면 방향을 확인한다. 이때 left이므로  tblCustomer에서 데이터를 찾아서 '호호호'와 '이순신'을 출력한다.
+		
+
+--right 조인
+SELECT
+	*
+FROM tblCustomer c
+	RIGHT outer JOIN tblsales s
+		ON c.seq = s.cseq; --9 (내부 조인과 동일한 결과)
+
+-- 방향이 right일 때, '호호호'와 '이순신' 데이터가 없는 이유는 tblsales에서 포함 안 된 데이터를 찾기 때문이다.
+-- 이처럼 외부 조인은 부모 테이블을 가리킬 때와 자식 테이블을 가리킬 때 결과에 차이가 있다.	
+		
+		
+SELECT * FROM tblStaff; --3명
+SELECT * FROM tblProject; --6건
+
+UPDATE tblProject SET staff_seq = 4 WHERE staff_seq = 3;
+
+-- 내부 조인
+SELECT
+	*
+FROM tblStaff s
+	INNER JOIN tblProject p
+		ON s.seq = p.staff_seq;
+-- 프로젝트를 1건 이상 담당하고 있는 직원
+
+-- 외부 조인
+SELECT
+	*
+FROM tblStaff s
+	LEFT OUTER JOIN tblProject p
+		ON s.seq = p.staff_seq;
+-- 담당 프로젝트의 유무와 상관없이 모든 직원
+	
+	
+	
+SELECT
+	*
+FROM tblVideo v
+	INNER JOIN tblRent r
+		ON v.seq = r.video;
+-- 대여가 한 번이라도 발생한 비디오와 대여 기록
+	
+SELECT
+	*
+FROM tblVideo v
+	LEFT outer JOIN tblRent r
+		ON v.seq = r.video;
+	
+SELECT
+	*
+FROM tblMember m
+	LEFT OUTER JOIN tblRent r
+		ON m.seq = r.MEMBER
+			WHERE r.seq IS NULL;
+-- 대여를 한 번도 하지 않은 고객 명단
+	
+SELECT
+	DISTINCT m.name
+FROM tblMember m
+	LEFT OUTER JOIN tblRent r
+		ON m.seq = r.MEMBER;
+
+	
+SELECT
+	m.name,
+	count(r.seq)
+FROM tblMember m
+	LEFT OUTER JOIN tblRent r
+		ON m.seq = r.MEMBER
+			GROUP BY m.name
+				ORDER BY count(r.seq) DESC;
+-- 대여 기록이 있는 회원의 이름 + 대여 횟수
+-- count를 와일드 카드로 쓰면 안 된다. rent 데이터와 관련된 r.seq로 카운트를 해줘야 한다.
+
+		
+
+
+
+			
+			
+/*			
+			
+	4. 셀프 조인 (SELF JOIN)
+	- 셀프 조인(Self Join)은1개의 테이블을 사용하는 조인이다.
+	- 셀프 조인은 자기 스스로와 관계를 맺는 경우를 의미한다.		
+	
+- 다중 조인(2개) + 내부 조인
+- 다중 조인(2개) + 외부 조인
+
+- 셀프 조인(1개) + 내부 조인
+- 셀프 조인(1개) + 외부 조인
+*/			
+	
+-- 직원 테이블
+CREATE TABLE tblSelf (
+
+	seq NUMBER PRIMARY KEY,						--직원번호(PK)
+	name varchar2(30) NOT NULL,					--직원명
+	department varchar2(30) NOT NULL,			--부서명
+	super NUMBER NULL REFERENCES tblSelf(seq)	--상사번호(FK)
+
+);
+
+-- super는 직속 상사를 저장한다. 직속 상사 또한 직원 중 한 명이므로 다른 사람의 시퀀스를 넣기로 한다.
+-- 이때도 똑같은 직원번호가 들어가는데, 다른 점은 상사의 번호라는 점이다.
+
+INSERT INTO tblSelf VALUES (1, '홍사장', '사장', null);
+INSERT INTO tblSelf VALUES (2, '김부장', '영업부', 1);
+INSERT INTO tblSelf VALUES (3, '박과장', '영업부', 2);
+INSERT INTO tblSelf VALUES (4, '최대리', '영업부', 3);
+INSERT INTO tblSelf VALUES (5, '정사원', '영업부', 4);
+INSERT INTO tblSelf VALUES (6, '이부장', '개발부', 1);
+INSERT INTO tblSelf VALUES (7, '하과장', '개발부', 6);
+INSERT INTO tblSelf VALUES (8, '신과장', '개발부', 6);
+INSERT INTO tblSelf VALUES (9, '황대리', '개발부', 7);
+INSERT INTO tblSelf VALUES (10, '허사원', '개발부', 9);
+
+COMMIT;
+SELECT * FROM tblSelf;
 
 
 
 
+-- 직원 명단을 가져오시오. 단, 상사의 이름까지 가져오시오.
+-- 1. 조인을 쓰는 방법
+-- 2. 서브 쿼리를 쓰는 방법
+-- 3. 계층형 쿼리를 쓰는 방법
+-- 직원 명단을 가져오려고 할 때, 위 3가지 방법을 사용할 수 있다.
+
+-- 1. 조인을 쓰는 방법
+SELECT
+	b.name AS 직원명,
+	b.department AS 부서명,
+	b.name AS 상사명
+FROM TBLSELF a				--역할: 부모테이블 > 상사
+	INNER JOIN TBLSELF b	--역할: 자식테이블 > 직원
+		ON a.seq = b.super;
+
+SELECT
+	b.name AS 직원명,
+	b.department AS 부서명,
+	b.name AS 상사명
+FROM TBLSELF a					--역할: 부모테이블 > 상사
+	RIGHT OUTER JOIN TBLSELF b	--역할: 자식테이블 > 직원
+		ON a.seq = b.super;
+
+	
+	
+-- 2. 서브 쿼리를 쓰는 방법
+SELECT
+	name AS 직원명,
+	department AS 부서명,
+	(SELECT name FROM tblSelf WHERE seq = a.super) AS 상사명
+FROM tblSelf a;
 
 
 
+/*
+	5. 전체 외부 조인 (FULL OUTER JOIN)
+	- 전체 외부 조인은 서로 참조하고 있는 관계에서 사용한다.
+	- 사용 빈도가 낮은 조인에 속한다.
 
 
+*/
 
+SELECT * FROM tblMen;	--부모, 자식
+SELECT * FROM tblWomen;	--자식, 부모
 
+-- 위 2개의 테이블은 서로 참조를 하는 관계이다
+-- 이처럼 서로 참조를 하고 있는 경우 어떻게 하느냐에 따라서 부모, 자식 관계가 서로 바뀔 수 있다.
+
+SELECT 
+	m.name,
+	w.name
+FROM tblMen m
+	INNER JOIN tblWomen w
+		ON m.name = w.couple;
+	
+-- 커플인 남자와 여자만을 가져왔다.
+	
+
+SELECT 
+	m.name,
+	w.name
+FROM tblMen m
+	LEFT OUTER JOIN tblWomen w
+		ON m.name = w.couple;
+
+-- 남자가 10명이 다 나왔다.
+-- 그리고 외부 조인이므로 남자 중에 여자친구가 있을 경우에도 가져왔다.
+	
+SELECT 
+	m.name,
+	w.name
+FROM tblMen m
+	RIGHT OUTER JOIN tblWomen w
+		ON m.name = w.couple;
+
+-- 이번에는 오른쪽 조인을 하였다.
+-- 여자가 10명이 다 나왔다.
+
+	
+SELECT 
+	m.name,
+	w.name
+FROM tblMen m
+	FULL OUTER JOIN tblWomen w
+		ON m.name = w.couple;
+		
+-- FULL이라고 적을 수 있다.
+-- 즉, 왼쪽 외부 조인과 오른쪽 외부 조인을 합친 것이다.
